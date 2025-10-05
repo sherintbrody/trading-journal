6969f3ec4e7d2846df306bc3cb07adc56db97e05
sherintbrody <42165562+sherintbrody@users.noreply.github.com>
2025-10-05 23:48:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", sco
 client = gspread.authorize(creds)
 
 # 📄 Load Existing Sheet
-sheet = client.open("Journal October competition").Log  # Replace with your actual sheet name
+sheet = client.open("Journal October competition").worksheet("Log") # Replace with your actual sheet name
 data = sheet.get_all_records()
 df = pd.DataFrame(data)
 
